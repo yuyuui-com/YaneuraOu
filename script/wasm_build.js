b8f357ccdfb9c4bd5f994d11e39de09fb1541306
YUYU <110108254+yuyu-ryokucha@users.noreply.github.com>
2026-02-26 17:15:04 +0900

    Remove USI book and eval file handling
    Removed environment variable handling for USI book and eval files.

--- script/wasm_build.js
+++ script/wasm_build.js
@@ -127,15 +127,12 @@ for(const pkgobj of pkglist) {
   // usi.js
   const bpath_usijs = fpath.join(cwd, builddirusi, `usi.${pkgobj.name}.js`);
   const bpath_usits = fpath.join(cwd, builddirusi, `usi.${pkgobj.name}.ts`);
-  fs.writeFileSync(bpath_usijs, `#!/usr/bin/env node
+fs.writeFileSync(bpath_usijs, `#!/usr/bin/env node
 const process = require("process");
 const fs = require("fs");
 const path = require("path");
 const readline = require("readline");
 const YaneuraOu = require("./lib/yaneuraou.${pkgobj.name}");
-
-const USI_BOOK_FILE = process.env.USI_BOOK_FILE;
-const USI_EVAL_FILE = process.env.USI_EVAL_FILE;
 
 async function runRepl(yaneuraou) {
   const iface = readline.createInterface({ input: process.stdin });
@@ -151,21 +148,7 @@ async function runRepl(yaneuraou) {
 async function main(argv) {
   const wasmBinary = await fs.promises.readFile(path.join(__dirname, "./lib/yaneuraou.${pkgobj.name}.wasm"));
   const yaneuraou = await YaneuraOu({ wasmBinary });
-  const FS = yaneuraou.FS;
-  if (USI_BOOK_FILE) {
-    const buffer = await fs.promises.readFile(USI_BOOK_FILE);
-    FS.writeFile(\`/\${path.basename(USI_BOOK_FILE)}\`, buffer);
-    yaneuraou.postMessage("setoption name BookDir value .");
-    yaneuraou.postMessage(\`setoption name BookFile value \${path.basename(USI_BOOK_FILE)}\`);
-  }
-  const USE_EVAL_FILE = ${pkgobj.evalfile};
-  if (USE_EVAL_FILE && USI_EVAL_FILE) {
-    const buffer = await fs.promises.readFile(USI_EVAL_FILE);
-    const filebase = path.basename(USI_EVAL_FILE);
-    FS.writeFile(\`/\${filebase}\`, buffer);
-    yaneuraou.postMessage("setoption name EvalDir value .");
-    yaneuraou.postMessage(\`setoption name EvalFile value \${filebase}\`);
-  }
+  
   if (argv.length > 0) {
     const commands = argv.join(" ").split(" , ");
     for (const command of commands) {
@@ -181,16 +164,13 @@ if (require.main === module) {
   main(process.argv.slice(2));
 }
 `);
-  fs.writeFileSync(bpath_usits, `/// <reference types="emscripten" />
+fs.writeFileSync(bpath_usits, `/// <reference types="emscripten" />
 import process from "process";
 import fs from "fs";
 import path from "path";
 import readline from "readline";
 import YaneuraOu = require("./lib/yaneuraou.${pkgobj.name}");
 import { YaneuraOuModule } from "./lib/yaneuraou.module";
-
-const USI_BOOK_FILE = process.env.USI_BOOK_FILE;
-const USI_EVAL_FILE = process.env.USI_EVAL_FILE;
 
 async function runRepl(yaneuraou: YaneuraOuModule) {
   const iface = readline.createInterface({ input: process.stdin });
@@ -206,21 +186,7 @@ async function runRepl(yaneuraou: YaneuraOuModule) {
 async function main(argv: string[]) {
   const wasmBinary = await fs.promises.readFile(path.join(__dirname, "./lib/yaneuraou.${pkgobj.name}.wasm"));
   const yaneuraou: YaneuraOuModule = await YaneuraOu({ wasmBinary });
-  const FS = yaneuraou.FS;
-  if (USI_BOOK_FILE) {
-    const buffer = await fs.promises.readFile(USI_BOOK_FILE);
-    FS.writeFile(\`/\${path.basename(USI_BOOK_FILE)}\`, buffer);
-    yaneuraou.postMessage("setoption name BookDir value .");
-    yaneuraou.postMessage(\`setoption name BookFile value \${path.basename(USI_BOOK_FILE)}\`);
-  }
-  const USE_EVAL_FILE = ${pkgobj.evalfile};
-  if (USE_EVAL_FILE && USI_EVAL_FILE) {
-    const buffer = await fs.promises.readFile(USI_EVAL_FILE);
-    const filebase = path.basename(USI_EVAL_FILE);
-    FS.writeFile(\`/\${filebase}\`, buffer);
-    yaneuraou.postMessage("setoption name EvalDir value .");
-    yaneuraou.postMessage(\`setoption name EvalFile value \${filebase}\`);
-  }
+  
   if (argv.length > 0) {
     const commands = argv.join(" ").split(" , ");
     for (const command of commands) {
