389672d77329db9909e3e8c7378aed64ea89bef2
YUYU <110108254+yuyu-ryokucha@users.noreply.github.com>
2026-02-26 18:15:03 +0900

    Update wasm_opfs.cpp

--- source/wasm_opfs.cpp
+++ source/wasm_opfs.cpp
@@ -1,5 +1,7 @@
 #include <emscripten/wasmfs.h>
+#include <emscripten.h>
 #include <iostream>
+#include "misc.h"
 
 __attribute__((constructor))
 void setup_wasmfs_opfs() {
@@ -11,5 +13,13 @@ void setup_wasmfs_opfs() {
         std::cout << "[WasmFS] OPFS successfully mounted at /opfs" << std::endl;
     } else {
         std::cerr << "[WasmFS] Failed to mount OPFS" << std::endl;
+    }
+}
+
+extern "C" {
+    EMSCRIPTEN_KEEPALIVE
+    int usi_command(const char* cmd) {
+        YaneuraOu::CommandLine::g.add_command(cmd);        
+        return 0; 
     }
 }
